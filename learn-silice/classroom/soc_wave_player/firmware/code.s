e3b3fd25714849437b6fda26b31fe55d5287e233
med-habbi <simo02096@gmail.com>
2025-12-02 17:36:34 +0100

    adjusting volume and frequency in step1_test

--- learn-silice/classroom/soc_wave_player/firmware/code.s
+++ learn-silice/classroom/soc_wave_player/firmware/code.s
@@ -17,11 +17,40 @@ Disassembly of section .text:
 
 0000001c <main>:
   1c:	000007b7          	lui	a5,0x0
-  20:	0347a703          	lw	a4,52(a5) # 34 <SNDGEN>
-  24:	000127b7          	lui	a5,0x12
-  28:	7ff78793          	addi	a5,a5,2047 # 127ff <__stacktop+0x27ff>
-  2c:	00f72023          	sw	a5,0(a4)
-  30:	00008067          	ret
+  20:	0907a503          	lw	a0,144(a5) # 90 <SNDGEN>
+  24:	000007b7          	lui	a5,0x0
+  28:	0947a603          	lw	a2,148(a5) # 94 <BUTTONS>
+  2c:	00018737          	lui	a4,0x18
+  30:	000007b7          	lui	a5,0x0
+  34:	0987a583          	lw	a1,152(a5) # 98 <LEDS>
+  38:	02000813          	li	a6,32
+  3c:	12700793          	li	a5,295
+  40:	04000893          	li	a7,64
+  44:	69f70713          	addi	a4,a4,1695 # 1869f <__stacktop+0x869f>
+  48:	00879693          	slli	a3,a5,0x8
+  4c:	0ff6e693          	ori	a3,a3,255
+  50:	00d52023          	sw	a3,0(a0)
+  54:	00062683          	lw	a3,0(a2)
+  58:	0106f693          	andi	a3,a3,16
+  5c:	00068663          	beqz	a3,68 <main+0x4c>
+  60:	12778793          	addi	a5,a5,295
+  64:	0105a023          	sw	a6,0(a1)
+  68:	00062683          	lw	a3,0(a2)
+  6c:	0206f693          	andi	a3,a3,32
+  70:	00068663          	beqz	a3,7c <main+0x60>
+  74:	ed978793          	addi	a5,a5,-295
+  78:	0115a023          	sw	a7,0(a1)
+  7c:	c0002373          	rdcycle	t1
+  80:	c00026f3          	rdcycle	a3
+  84:	406686b3          	sub	a3,a3,t1
+  88:	fed77ce3          	bgeu	a4,a3,80 <main+0x64>
+  8c:	fbdff06f          	j	48 <main+0x2c>
 
-00000034 <SNDGEN>:
-  34:	00010200                                ....
+00000090 <SNDGEN>:
+  90:	00010200                                ....
+
+00000094 <BUTTONS>:
+  94:	00010100                                ....
+
+00000098 <LEDS>:
+  98:	00010004                                ....
--- learn-silice/classroom/soc_wave_player/firmware/step1_test.c
+++ learn-silice/classroom/soc_wave_player/firmware/step1_test.c
@@ -11,10 +11,47 @@
 
 #include "config.h"
 #include "std.h"
+#include <stdbool.h>
+
+
 
 void main()
 {
-    int inc_per_cycle=(1<<24)/ 56818;
- *SNDGEN = (inc_per_cycle <<8)| 255;
+  int inc_per_cycle=(1<<24)/ 56818;
 
+ bool vol = false;
+ int ampl=0;
+while(1){ 
+    //*SNDGEN = (inc_per_cycle <<8)| ampl;
+
+    //ajuster le volume avec les boutons
+// *LEDS = 1<<1;
+//     if (*BUTTONS & (1<<3)) {
+//         vol= !vol;
+//         *LEDS = 1<<2;
+//         }  
+//         if(vol){
+//             ampl=255;
+//         }else{
+//             ampl=0;
+//         }
+//        *LEDS = 1<<4;
+//     }
+ 
+// ajuster frequence avec les boutons
+*SNDGEN = (inc_per_cycle <<8)| 255;
+    if (*BUTTONS & (1<<4)) {
+        inc_per_cycle+= (1<<24)/ 56818;
+        *LEDS = 1<<5;
+        }  
+        if (*BUTTONS & (1<<5)) {
+        inc_per_cycle-= (1<<24)/ 56818;
+        *LEDS = 1<<6;
+        } 
+        if (*BUTTONS & (1<<3)) {
+//         vol= !vol;
+//         *LEDS = 1<<2;
+//         }  
+    pause(100000);
+}
 }
